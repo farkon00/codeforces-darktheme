e018ec12a2f0c48481ac9d7077eb001894fbd13b
LordLava <davidstarup@gmail.com>
2022-07-06 14:56:27 +0300

    Changed Expert Colour

--- codeforces-darktheme-darknutella.user.js
+++ codeforces-darktheme-darknutella.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Codeforces Dark mode + Dark nutella
-// @version      1.1.10
+// @version      1.1.10.1
 // @description  Improved dark mode for Codeforces
 // @author       Gaurang Tandon & fork by David
 // @match        https://codeforces.com/*
