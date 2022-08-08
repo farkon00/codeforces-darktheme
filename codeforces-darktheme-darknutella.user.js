5d8bb734270ff7aa5b60a46bce6ee18e9cf91682
LordLava <davidstarup@gmail.com>
2022-08-08 23:45:46 +0300

    Fixed codeforces messaging being to bright and fixed the new test case feature being too bright

--- codeforces-darktheme-darknutella.user.js
+++ codeforces-darktheme-darknutella.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Codeforces Dark mode + Dark nutella
-// @version      1.1.10.1
+// @version      1.2
 // @description  Improved dark mode for Codeforces
 // @author       Gaurang Tandon & fork by David
 // @match        https://codeforces.com/*
