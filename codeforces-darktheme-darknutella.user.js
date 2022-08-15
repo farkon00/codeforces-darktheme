c1b10d4497709066201e93d5595af0d9030691b2
LordLava <davidstarup@gmail.com>
2022-08-15 18:23:35 +0300

    Possible fix to colours

--- codeforces-darktheme-darknutella.user.js
+++ codeforces-darktheme-darknutella.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Codeforces Dark mode + Dark nutella
-// @version      1.2
+// @version      1.2.1
 // @description  Improved dark mode for Codeforces
 // @author       Gaurang Tandon & fork by David
 // @match        https://codeforces.com/*
--- codeforces-darktheme.user.js
+++ codeforces-darktheme.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Codeforces dark mode
-// @version      1.2
+// @version      1.2.1
 // @description  Improved dark mode for Codeforces
 // @author       Gaurang Tandon & fork by David
 // @match        https://codeforces.com/*
